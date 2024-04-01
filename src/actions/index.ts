377958199d1120c01c15f2dad83ba494f92ba636
Alex <puskin1914@gmail.com>
2024-04-01 22:14:26 +0400

    use useFormState hook for validation

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -25,3 +25,34 @@ export const deleteSnippet = async (id: number) => {
 
   redirect('/');
 };
+
+export const createSnippet = async (
+  formState: { message: string },
+  formData: FormData
+) => {
+  const title = formData.get('title');
+  const code = formData.get('code');
+
+  if (typeof title !== 'string' || title.length < 3) {
+    return {
+      message: 'Title must be at least 3 characters',
+    };
+  }
+
+  if (typeof code !== 'string' || code.length < 10) {
+    return {
+      message: 'Code must be at least 10 characters',
+    };
+  }
+
+  const snippet = await db.snippet.create({
+    data: {
+      title,
+      code,
+    },
+  });
+
+  console.log(snippet);
+
+  redirect('/');
+};
